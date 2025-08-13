bd09f6fefb597084cc791d84f02d132d3345dcc9
Karthikraja16 <karthikrajavm@gmail.com>
2025-08-13 14:56:50 +0530

    update

--- src/main/java/com/demo/BootController.java
+++ src/main/java/com/demo/BootController.java
@@ -1,5 +1,21 @@
 package com.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BootController {
+	@GetMapping(value ="/get")
+	public String get() {
+		return "karthikraja";
+	}
+	@GetMapping(value="/get1")
+	public long get1() {
+		return 8220196738l;
+	}
+	@GetMapping(value="/get2")
+	public String get2() {
+		return "DinaKaran";
+	}
 
 }
